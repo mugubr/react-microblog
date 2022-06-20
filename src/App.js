edb1306edf5add5fcb6e467d18b838207a3cdeb3
mugubr <miguimuniz@gmail.com>
2022-06-20 11:38:04 -0300

    Sidebar and Body(props)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
+import Body from './components/Body';
+import Posts from './components/Posts';
 
 export default function App() {
-  const posts = [
-    {
-      id: 1,
-      text: 'Hello, world!',
-      timestamp: 'a minute ago',
-      author: {
-        username: 'susan',
-      },
-    },
-    {
-      id: 2,
-      text: 'Second post',
-      timestamp: 'an hour ago',
-      author: {
-        username: 'john',
-      },
-    },
-  ];
-
   return (
-    <Container fluid className='App'>
+    <Container fluid className="App">
       <Header />
-      <Container>
-        {posts.length === 0 ?
-          <p>There are no blog posts.</p>
-          :
-          posts.map(post => {
-            return (
-              <p key={post.id}>
-                <b>{post.author.username}</b> &mdash; {post.timestamp}
-                <br />
-                {post.text}
-              </p>
-            );
-          })
-        }
-      </Container>
+      <Body sidebar>
+        <Posts />
+      </Body>
     </Container>
   );
 }
